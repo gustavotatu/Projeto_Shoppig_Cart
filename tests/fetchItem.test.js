f3560b9cd21d1d8670f0ca49ce6351db5b2d923c
Gustavo Pereira <gupersil04@gmail.com>
2022-10-14 03:22:31 -0300

    Requisitos 1 e 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,11 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Testa se fetchItem é uma função', () => {
+    expect(fetchItem).toBeInstanceOf(Function);
+  });
+  test('Ao chamar a função fetchItem com um id, a função fetch é chamada', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,26 @@
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
+const fetchSimulator = require('../mocks/fetchSimulator');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Testa se fetchProducts é uma função', () => {
+    expect(fetchProducts).toBeInstanceOf(Function);
+  });
+  test('Ao executar a função fetchProducts com o argumento computador, a função fetch é chamada', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  }); 
+  test('Ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint correto', () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  test('Ao chamar a função fetchProducts com o argumento computador, a função retorna a estrutura de dados esperada', () => {
+    expect(fetchProducts('computador')).toEqual(computadorSearch);
+  });
+/*   test('Ao chamar a função fetchProducts sem argumento, retorna um erro', () => {
+    expect(fetchProducts()).toEqual(('You must provide an url'));
+  });  */
 });
