da2e4d48b08bca85b8c2ad7d28464baff9387ce2
Gustavo Pereira <gupersil04@gmail.com>
2022-10-14 21:08:28 -0300

    Requisito 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
+const fetchSimulator = require('../mocks/fetchSimulator');
+const computadorSearch = require('../mocks/search');
+const { results } = require('../mocks/search');
+
+const fetchProducts = (computador) => {
   // seu código aqui
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${computador}`;
+  fetch(url);
+  return computadorSearch;
 };
 
 if (typeof module !== 'undefined') {
