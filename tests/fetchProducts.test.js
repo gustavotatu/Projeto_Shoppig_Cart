21943b8e15018dd0d7f423e76d550301d21f2101
Gustavo Pereira <gupersil04@gmail.com>
2022-10-18 00:23:55 -0300

    Requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -12,4 +12,16 @@ describe('1 - Teste a função fetchProducts', () => {
     fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
   });
+  it('Testa se ao chamar fetchProducts com o parâmetro computador, fetch utiliza o endpoint correto', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  it('Testa se o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const ret = await fetchProducts('computador');
+    expect(ret).toEqual(computadorSearch);
+  });
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+    const ret = await fetchProducts();
+    expect(ret).toEqual(new Error('You must provide an url'));
+  });
 });
