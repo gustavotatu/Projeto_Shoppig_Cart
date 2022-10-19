66d1108723b87780a8c866de4459de2232e8aad4
Gustavo Pereira <gupersil04@gmail.com>
2022-10-18 23:02:00 -0300

    Requisito 6 e 7

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,12 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', () => {
+    saveCartItems('ID: MLB2711430947 | TITLE: Notebook Multilaser Legacy Book Pc310 Preta 14.1 , Intel Celeron N3000  4gb De Ram 64gb Ssd, Intel Hd Graphics 1366x768px Windows 10 Home | PRICE: $1187');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Testa se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave cartItems e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    saveCartItems('ID: MLB2711430947 | TITLE: Notebook Multilaser Legacy Book Pc310 Preta 14.1 , Intel Celeron N3000  4gb De Ram 64gb Ssd, Intel Hd Graphics 1366x768px Windows 10 Home | PRICE: $1187');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartiItems', 'ID: MLB2711430947 | TITLE: Notebook Multilaser Legacy Book Pc310 Preta 14.1 , Intel Celeron N3000  4gb De Ram 64gb Ssd, Intel Hd Graphics 1366x768px Windows 10 Home | PRICE: $1187');
+  });
 });
